0a0932232adb2c873aca4e99e3377091f67875f6
disensin <disensin@gmail.com>
2016-12-12 11:47:56 -0800

    Update TrackMeWontYou.py

--- TrackMeWontYou.py
+++ TrackMeWontYou.py
@@ -1,36 +1,20 @@
 import maya.cmds as mc
+import mAnimLib.mConLib
 
-selectedObjects = mc.ls(sl = 1)
-
-def cubeMe(getMe):
-    splinedCube = mc.curve(
-        degree = 1,
-        point = [
-            (1, 1, 1),
-            (1,-1,1),
-            (1,-1,-1),
-            (1,1,-1),
-            (-1,1,-1),
-            (-1,-1,-1),
-            (-1,-1,1),
-            (-1,1,1),
-            (1,1,1),
-            (1,-1,1),
-            (-1,-1,1),
-            (-1,1,1),
-            (-1,1,-1),
-            (-1,-1,-1),
-            (1,-1,-1),
-            (1,1,-1),
-            (1,1,1)
-        ],
-        knot = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16))
-
-    splinedCube = mc.rename(splinedCube, getMe+'_curveCube')
-    mc.matchTransform(splinedCube, getMe)
-    return splinedCube
-
+# Parent Target Object to cube3. If a Parent Constraint doesn't work...
+def forcedParenting(daddy,sonny):
+    try:
+        mc.parentConstraint( daddy, sonny, weight = 1)
+    except:
+        # ...it will try a Point Constraint. If THAT doesn't work...
+        try:
+            mc.pointConstraint( daddy, sonny, weight = 1)
+        # ...do an Orient Constraint!
+        except:
+            mc.orientConstraint( daddy, sonny, weight = 1)
+                
 def bakeOff(self):
+    mc.select(self)
     mc.refresh(suspend=True)
 
     # Get all the frames in the scene.
@@ -60,26 +44,24 @@ def bakeOff(self):
     mc.select(self)
     mc.delete(cn = 1)
 
-targetCubeOffset = cubeMe(selectedObjects[0])
-targetCubeOffset = mc.rename(targetCubeOffset,
-                             (selectedObjects[0] + '_offset_curveCube'))
+cubeList = []
+targetList = mc.ls(sl = 1)
+target = targetList[0]
 
+mAnimLib.mConLib.createCubeCON()
+cubeOne = mc.rename(target + '_cube1')
+cubeList += [cubeOne]
 
-targetCubeBaked = cubeMe(selectedObjects[0])
-mc.scale(.8,.8,.8, targetCubeBaked)
-targetCubeBaked = mc.rename(targetCubeBaked,
-                            (selectedObjects[0] + '_baked_curveCube'))
+mAnimLib.mConLib.createCubeCON()
+cubeTwo = mc.rename(target + '_cube2')
+mc.scale(0.8,0.8,0.8)
+cubeList += [cubeTwo]
 
-if len(selectedObjects) == 2:
-    targetCubeDad = cubeMe(selectedObjects[1])
-    targetCubeDad = mc.rename(targetCubeDad,
-                              (selectedObjects[0] + '_dad_curveCube'))
+mc.parent(cubeTwo,cubeOne)
 
+mc.parentConstraint(target,cubeTwo)
+mc.parentConstraint(target, cubeOne, mo = True, skipTranslate = 'y', weight = 1)
 
-mc.parent(targetCubeBaked,targetCubeOffset)
-if len(selectedObjects) == 2:
-    mc.parent(targetCubeOffset,targetCubeDad)
+bakeOff(cubeList)
 
-mc.parentConstraint(selectedObjects[0],targetCubeBaked)
-
-bakeOff(targetCubeBaked)
+forcedParenting(cubeTwo, target)
