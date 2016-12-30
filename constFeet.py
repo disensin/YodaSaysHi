50b1ebb112f8b9c4b17c95577f3a46a057fd7b98
disensin <disensin@gmail.com>
2016-12-29 18:40:35 -0800

    Update constFeet.py

--- constFeet.py
+++ constFeet.py
@@ -26,11 +26,30 @@ def snapTo(pops,kid):
     mc.parentConstraint(pops,kid,mo=0)
     mc.select(mc.listRelatives(c=1,typ = 'constraint'))
     mc.delete()
-
+    
+# Set "self" Y translation to the default falue
 def yLimitMe(self):
     minVal = mc.getAttr(str(self + '.translateY'))
     mc.transformLimits ( self, ty = (minVal, 1), ety = (1 ,0))
 
+# Create a null and snap to the item. Name it first.
+def makeNull(nullName,item): # Include nullName and the item to snap it to.
+    nullItem = mc.group(em=True, name=(item + '_' + nullName ))
+    snapTo(item,nullItem)
+    return nullItem
+
+# Parent son to dad
+def forceParent(dad,son, mOffset = 0):
+    # Parent Target Object to cube3. If a Parent Constraint doesn't work...
+    try:
+        mc.parentConstraint( dad, son, weight = 1, mo = mOffset)
+    except:
+        # ...it will try a Point Constraint. If THAT doesn't work...
+        try:
+            mc.pointConstraint( dad, son, weight = 1, mo = mOffset)
+        # ...do an Orient Constraint!
+        except:
+            mc.orientConstraint( dad, son, weight = 1, mo = mOffset)
 
 # Get the foot list
 footList = addFeetHere('L')
@@ -57,10 +76,10 @@ headGroup = mc.group(em=True, name = (footList[0] + '_flatFootted_rig')) # Paren
 # Create a locator for each item and store in the "footListLOC". Parent each one under "headGroup"
 for i in range(len(footList)):
     item = footList[i]
-    nullItem = mc.group(em=True, name=(item + '_null' ))
-    snapTo(item,nullItem)
+    nullItem = makeNull('yPenNull',item)
     mc.parent(nullItem, headGroup)
     footListLOC += [nullItem]
+
 
 # Get each new locator labeled
 footLOC = footListLOC[0]
@@ -70,15 +89,28 @@ pinkyLOC = footListLOC[3]
 biggieLOC = footListLOC[4]
 heelLOC = footListLOC[5]
 
-# Ball setup.
-mc.parentConstraint(ballLOC,foot,mo=1)
 FootMcNUL = mc.pickWalk(foot,d='up')
-mc.parentConstraint(FootMcNUL,ballLOC,mo=1)
+BallMcNUL = mc.pickWalk(ball,d='up')
+ToeMcNUL = mc.pickWalk(toe,d='up')
+
+# Constrain foot to heel
+mc.parentConstraint(heelLOC,foot,mo=1)
+mc.parentConstraint(FootMcNUL,heelLOC,mo=1)
+yLimitMe(heelLOC)
+
+# Constrain ball to ballCON
+forceParent(ballLOC,ball)
+mc.parentConstraint(BallMcNUL,ballLOC)
 yLimitMe(ballLOC)
 
-# Heel setup. pConst from: footLOC to BallLOC toFootMcNUL
-mc.parentConstraint(FootMcNUL,heelLOC,mo=1)
-mc.aimConstraint(heelLOC,ballLOC,mo=1)
+# Constrain toe to toeNUL
+forceParent(toeLOC,toe)
+mc.orientConstraint(toeLOC,toe,skip=['y','z'])
+mc.parentConstraint(ToeMcNUL,toeLOC)
+yLimitMe(toeLOC)
 
-
+toeAIM = makeNull('subToe',toeLOC)
+mc.move(.5,y=1)
+mc.parent(toeAIM,headGroup)
+mc.parentConstraint(ToeMcNUL,toeAIM,mo=1)
 
