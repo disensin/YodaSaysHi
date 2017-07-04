24e04bab9b0a6fc1affb93083ae974acabaed000
disensin <disensin@gmail.com>
2017-07-03 22:53:12 -0700

    Update...
    Improved the __init__ to run cleaner. Enjoy!

--- pivotic.py
+++ pivotic.py
@@ -1,17 +1,11 @@
 '''
 To run, place script in your Scripts folder, then use this code:
 
-import pivotic
-reload(pivotic)
+import pivotic;reload(pivotic);pivotic.PivotIC().createPivot() # This code creates a pivot on the selected item
 
-foo = pivotic.PivotIC()
-foo.createPivot() # This code creates a pivot on the selected item
+import pivotic;reload(pivotic);pivotic.PivotIC().setKeyOnMainItem() # This code sets a key on the item that is being controlled
 
-foo = pivotic.PivotIC()
-foo.setKeyOnMainItem() # This code sets a key on the item that is being controlled
-
-foo = pivotic.PivotIC()
-foo.removePivotTool() # This code removes the pivot tool.
+import pivotic;reload(pivotic);pivotic.PivotIC().removePivotTool() # This code removes the pivot tool.
 
 '''
 
@@ -24,37 +18,34 @@ class PivotIC(object):
         Initialize the Class variable. In this case, get the selected objects.
         If the selected object already has a Pivot Tool applied on it, load the variables.
         '''
-        if mc.ls(sl=1): # if there's a selection, engage the variables
+        if len(mc.ls(sl=1)) == 0 : # If nothing is selected, create the variables.
+            self.pivotGroup,self.diamondCtrl,self.pivotCtrl,self.basePivotAnchor='','','',''
+            self.pivotItems = [self.pivotGroup,self.diamondCtrl,self.pivotCtrl,self.basePivotAnchor]
+
+        elif mc.ls(sl=1): # If there's a selection, engage the variables
             self.items = mc.ls(sl=1)
-            if mc.ls(self.items[0].split('_pivot')[0]) > 1:
-                self.items = mc.ls(self.items[0].split('_pivot')[0]) 
 
-            self.pivotList = mc.ls('*_pivotTool_*') # let's find any pivot tools out there...
-            for piv in self.pivotList: # ...and compare the selected object to them all.
-                newGuy = mc.getAttr(piv+'.ItemsInPivotTool').split() + self.pivotList
+            self.pivotList = mc.ls('*_pivotTool_*')
+            if len(self.items[0].split('_pivot'))>1: # Here we check if a selected item belongs to a pivot tool
+                self.items = [self.items[0].split('_pivot')[0]] # If it does, replace self.item with the actual object's name.
 
-                if self.items[0] in newGuy: # if the selected object is in one of the pivots
-                    self.items = newGuy # replace list with the original selection order
-                    break
+            if self.pivotList: # If there are multiple Pivots in the list,
+                for p in self.pivotList: # For every pivot in the list,
+                    if self.items[0] in mc.getAttr(p+'.ItemsInPivotTool').split(): # If the first item and first Attr in the too are the same,
+                        self.items = mc.getAttr(p+'.ItemsInPivotTool').split() # Replace self.item with the first item in the Attr
+                        break # stop searching.
 
-            # print self.items
-            if mc.ls(self.items[0]+"_pivotTool_*"):
-                self.diamondCtrl = mc.ls(self.items[0]+"_pivotDiamond_*")[0]
-                self.pivotCtrl = mc.ls(self.items[0]+"_pivotCenter_*")[0]
-                self.pivotGroup = mc.ls(self.items[0]+"_pivotTool_*")[0]
-                self.basePivotRef = mc.ls(self.items[0]+"_pivotRef_*")[0]
+            if mc.ls(self.items[0]+"_pivotTool_*"): # If the item already has a Pivot created,
+                self.diamondCtrl = mc.ls(self.items[0]+"_pivotDiamond_*")[0] # Apply the diamond control variable,
+                self.pivotCtrl = mc.ls(self.items[0]+"_pivotCenter_*")[0] # Apply the pivot control variable,
+                self.pivotGroup = mc.ls(self.items[0]+"_pivotTool_*")[0] # Apply the pivot group variable,
+                self.basePivotAnchor = mc.ls(self.items[0]+"_pivotAnchor_*")[0] # Apply the Ancor Locator constraint variable,
 
-                self.pivotList = [self.pivotGroup,self.diamondCtrl,self.pivotCtrl,self.basePivotRef]
+                self.pivotItems = [self.pivotGroup,self.diamondCtrl,self.pivotCtrl,self.basePivotAnchor] # Create the list of variables.
                 
-                self.items = mc.getAttr(self.pivotList[0]+'.ItemsInPivotTool').split()
-                print self.pivotList,self.items
-                mc.select(self.pivotList[1])
-            else:
-                pass
+                self.items = mc.getAttr(self.pivotItems[0]+'.ItemsInPivotTool').split() # Collect all the items that are involved with the current pivot.
+                mc.select(self.pivotItems[1]) # Select the current Pivot's diamond to manipulate
 
-        else:
-            self.pivotGroup,self.diamondCtrl,self.pivotCtrl,self.basePivotRef='','','',''
-            self.pivotList = [self.pivotGroup,self.diamondCtrl,self.pivotCtrl,self.basePivotRef]
 
     def listScenePivots(self):
         '''
@@ -66,18 +57,19 @@ class PivotIC(object):
     def makeNewPivotCtrl(self):
         '''
         Creates the following hierarchy:
-            New Pivot Group
-                New Diamond Control
-                    New Pivot Control
-                        New Pivot Group
+            New Pivot Group: stores all the Pivot Tool objects, keeps it all at 0,0,0.
+                New Diamond Control: use this to control the main object
+                    New Pivot Control: use this one to move the pivot around 
+                        New Anchor Locator Null: constrains all the items to this object.
 
         It returns a list with all these items.
         '''
         diamondName = self.items[0]+"_pivotDiamond_#"
         pivotName = self.items[0]+"_pivotCenter_#"
         pivotGroupName = self.items[0]+"_pivotTool_#"
-        basePivotRefName = self.items[0]+"_pivotRef_#"
+        basePivotAnchorName = self.items[0]+"_pivotAnchor_#"
 
+        # Create the first control, the Diamond. Apply a blue override color.
         self.diamondCtrl = mc.curve(n=diamondName,d=1,p=[ 
                                     (-1, 0, 0),
                                     (0 ,0 ,-1),
@@ -97,6 +89,7 @@ class PivotIC(object):
         mc.setAttr(self.diamondCtrl + '.overrideEnabled', 1)
         mc.setAttr(self.diamondCtrl + '.overrideColor', 6)
 
+        # Create the second control, the cross. Apply a light blue override color.
         self.pivotCtrl = mc.curve(n=pivotName,d=1,p=[
                                     (-1,0,0),
                                     (1,0,0),
@@ -112,32 +105,35 @@ class PivotIC(object):
                                     k=[0,1,2,3,4,5,6,7,8,9,10])
         mc.setAttr(self.pivotCtrl + '.overrideEnabled', 1)
         mc.setAttr(self.pivotCtrl + '.overrideColor', 18)
-
-
+        # Lock and hide the Rotations of the cross.
         mc.setAttr((self.pivotCtrl+".rx"), lock=True, keyable=False, channelBox=False)
         mc.setAttr((self.pivotCtrl+".ry"), lock=True, keyable=False, channelBox=False)
         mc.setAttr((self.pivotCtrl+".rz"), lock=True, keyable=False, channelBox=False)
 
-        self.pivotGroup = mc.group(name=pivotGroupName,em=1)
+        self.pivotGroup = mc.group(name=pivotGroupName,em=1) # Create the null to store all the objects.
 
-        self.basePivotRef = mc.group(name=basePivotRefName,em=1)
+        self.basePivotAnchor = mc.group(name=basePivotAnchorName,em=1) # Create the Anchor Locator to constrain the items
 
-        mc.parent(self.basePivotRef,self.pivotCtrl)
-        mc.parent(self.pivotCtrl,self.diamondCtrl)
-        mc.parent(self.diamondCtrl,self.pivotGroup)
+        mc.parent(self.basePivotAnchor,self.pivotCtrl) # Parent the constraint null under the pivot.
+        mc.parent(self.pivotCtrl,self.diamondCtrl) # Parent the pivot under the diamond.
+        mc.parent(self.diamondCtrl,self.pivotGroup) # Parent the diamond under the top null.
 
-        self.pivotList = [self.pivotGroup,self.diamondCtrl,self.pivotCtrl,self.basePivotRef]
+        self.pivotItems = [self.pivotGroup,self.diamondCtrl,self.pivotCtrl,self.basePivotAnchor] # Store the variables in a main list.
 
-        return self.pivotList
+        return self.pivotItems # Return the list of created objects.
 
     def snapToFirstItem(self,fromMe=None,toYou=None):
         '''
-        Snaps the first selected item to the second item.
+        INPUT: Snaps the first selected item to the second item. If there's a selection, it'll snap the second object to the first.
 
-        The items are then selected in the same order
+        OUTPUT: The items are then selected in the same order.
+
+        fromMe - string item, get the location from this one
+
+        toYou - string item, apply location to this one
         '''
 
-        if mc.ls(sl=1):
+        if mc.ls(sl=1): # If there's a selection
             fromMe = mc.ls(sl=1)[0]
             toYou = mc.ls(sl=1)[1]
         mc.xform(toYou, ro= (mc.xform(fromMe,ro=1,q=1,ws=1)), ws=1)
@@ -146,51 +142,57 @@ class PivotIC(object):
 
     def createPivot(self):
         '''
-        Creates a set of pivot controls for the selected items.
+        INPUT: Creates a set of pivot controls for the selected items.
 
-        Selects the movable control that allows positioning.
+        OUTPUT: Selects the movable control that allows repositioning of the pivot point.
         '''
-        if mc.ls(self.items[0]+"_pivotTool_*"):
-            print "This item already has a Pivot Control. Delete it first, then make a new one!"
+        if mc.ls(self.items[0]+"_pivotTool_*"): # If the current item already has a pivot, warn the user and select the diamond.
+            print("This item already has a Pivot Control. Delete it first, then make a new one!")
+            mc.select(self.pivotItems[1])
+
         else:
-            self.makeNewPivotCtrl()
-            print self.pivotList[0]
-            print self.items[0]
-            mc.select(cl=1)
-            self.snapToFirstItem(self.items[0],self.pivotList[0])
+            self.makeNewPivotCtrl() # Create a new Pivot Tool hierarchy
+            mc.select(cl=1) # Clear selection before running the snap tool.
+            self.snapToFirstItem(self.items[0],self.pivotItems[0]) # Snap the Pivot Group to the first item.
 
-            for item in self.items:
-                constraint = mc.parentConstraint(self.pivotList[3],item,mo=1,w=1)
+            for item in self.items: # Constrain every item to the Pivot Tool's anchor locator.
+                constraint = mc.parentConstraint(self.pivotItems[3],item,mo=1,w=1)
 
-            multDivNode = mc.shadingNode('multiplyDivide', asUtility=True)
-            mc.connectAttr((self.pivotList[2]+'.translate'), (multDivNode+'.input1'),f=1)
-            mc.setAttr(multDivNode+".input2Y",-1)
-            mc.setAttr(multDivNode+".input2X",-1)
-            mc.setAttr(multDivNode+".input2Z",-1)
-            mc.connectAttr((multDivNode+'.output'), (self.pivotList[3]+'.translate'),f=1)
-            mc.connectAttr((self.pivotList[2]+'.translate'), (self.pivotList[1]+'.rotatePivot'),f=1)
+            multDivNode = mc.shadingNode('multiplyDivide', asUtility=True) # Create a multiply/divide node,
+            mc.connectAttr((self.pivotItems[2]+'.translate'), (multDivNode+'.input1'),f=1) # connect the Pivot Locator to the MultDiv,
+            mc.setAttr(multDivNode+".input2X",-1) # Reverse the values of X.
+            mc.setAttr(multDivNode+".input2Y",-1) # Reverse the values of Y.
+            mc.setAttr(multDivNode+".input2Z",-1) # Reverse the values of Z.
+            mc.connectAttr((multDivNode+'.output'), (self.pivotItems[3]+'.translate'),f=1) # Connect the MultDiv to the Anchor Locator.
+            
+            # KEY MOMENT, connects the Diamond's rotatePivot to the Pivot Locator.
+            mc.connectAttr((self.pivotItems[2]+'.translate'), (self.pivotItems[1]+'.rotatePivot'),f=1)
 
-            mc.addAttr(self.pivotList[0],ln="ItemsInPivotTool",dt='string')
-            mc.setAttr((self.pivotList[0]+'.ItemsInPivotTool'), e=1,channelBox=True)
+            mc.addAttr(self.pivotItems[0],ln="ItemsInPivotTool",dt='string') # On the top Pivot Group, add an attribute that'll receive strings.
+            mc.setAttr((self.pivotItems[0]+'.ItemsInPivotTool'), e=1,channelBox=True)
+
             writeIn = ''
-            for item in self.items:
+            for item in self.items: # Create a string with all the items separated by a space " "...
                 writeIn += "{0} ".format(item)
                 
-            mc.setAttr((self.pivotList[0]+'.ItemsInPivotTool'),writeIn,type="string")
+            mc.setAttr((self.pivotItems[0]+'.ItemsInPivotTool'),writeIn,type="string") # and store them all in the top group for later use.
 
-            mc.select(self.pivotList[1])
+            print('Pivot Tool created. Enjoy!')
+            mc.select(self.pivotItems[1]) # Selects the diamond. Go play!
 
     def setKeyOnMainItem(self):
         '''
-        Set a key on the items driven by the objects controlled by the current pivot.
+        Set a key on the items driven by the objects controlled by the current pivot. Sets the "blendParent" values to 1.
         '''
-        print self.items, "will be key'd"
+        print("{0} will be key'd".format(self.items))
         mc.setKeyframe(self.items)
         for item in self.items:
             mc.setAttr((item+".blendParent1"), 1)
 
     def removePivotTool(self):
-        mc.delete(self.pivotList)
+        '''
+        Removes the Pivot Tool without needing to open the Outliner!
+        '''
+        mc.delete(self.pivotItems)
         print('Tool as been removed!')
 
-        
