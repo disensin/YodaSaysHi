ec3de6e0245e0a16efc08750dd008d1558dd5464
Isai Calderon <disensin@gmail.com>
2022-09-30 14:13:58 -0700

    Select Curve and more...
    Select Tracked node or MotionTracker node;
    get_tracker_node now deletes node if requested.

--- anchoredMotionTrail.py
+++ anchoredMotionTrail.py
@@ -7,16 +7,24 @@ def _get_camera():
     this_camera = pm.modelEditor(found_panel,q=1,av=1,cam=1)
     return this_camera
 
-def get_tracked_node():
+def get_tracked_node(select=False):
     if pm.objExists(TRACKER_NAME):
         tracker_node = pm.PyNode(TRACKER_NAME)
         tracker_node.inputMatrix.inputs()[0].select()
+        if select:
+            pm.select(tracker_node.inputMatrix.inputs()[0])
         return tracker_node.inputMatrix.inputs()[0]
     return False
 
-def delete_tracker_node():
+def get_tracker_node(delete=False, select=False):
     if pm.objExists(TRACKER_NAME):
-        pm.delete(TRACKER_NAME+'Handle')
+        if delete:
+            pm.delete(TRACKER_NAME+'Handle')
+            return False
+        if select:
+            pm.PyNode(TRACKER_NAME+'Handle').select()
+        return TRACKER_NAME+'Handle'
+    
 
 def make_tracker_node(items = None):
     items = items or pm.selected()
@@ -26,7 +34,7 @@ def make_tracker_node(items = None):
         pm.error('Select an object first!')
 
     item = items[0]
-    delete_tracker_node()
+    get_tracker_node(delete=True)
 
     motion_path  = pm.snapshot(item,
                     name=TRACKER_NAME,
@@ -36,5 +44,3 @@ def make_tracker_node(items = None):
                     startTime = pm.playbackOptions(q=1,min=1),
                     endTime = pm.playbackOptions(q=1,max=1))
     return motion_path
-
-
